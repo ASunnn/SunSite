8731aaa4d390545649699b80a083ce9fd013abf6
ASunnn <liurf923@gmaill.com>
2018-04-02 15:29:35 +0800

    完成tomcat部署简单设置

--- src/main/java/sunnn/sunsite/SunSiteApplication.java
+++ src/main/java/sunnn/sunsite/SunSiteApplication.java
@@ -2,11 +2,19 @@ package sunnn.sunsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SunSiteApplication {
+public class SunSiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SunSiteApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SunSiteApplication.class);
+	}
+
 }
