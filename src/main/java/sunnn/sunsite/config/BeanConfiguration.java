139e899b51f204e8d1c7c2720fcb31426319a588
ASun <liurf923@gmail.com>
2019-02-09 20:53:44 +0800

    1.4.0 update 開始增加檢索功能 修復了排序算法在極少數情況下發生錯誤的問題

--- src/main/java/sunnn/sunsite/config/BeanConfiguration.java
+++ src/main/java/sunnn/sunsite/config/BeanConfiguration.java
@@ -50,7 +50,7 @@ public class BeanConfiguration {
         String sys = System.getProperty("os.name");
 
         if (sys.contains("Windows")) {
-            return "C:\\ProgramData\\sunsite\\sunsite-dev.properties";
+            return "C:\\ProgramData\\sunsite\\sunsite.properties";
         } else if (sys.contains("Linux"))
             return "";
 
